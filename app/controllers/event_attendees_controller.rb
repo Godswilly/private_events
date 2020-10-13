4e20e78e964e3726e85912a9b43e22a5a6de7247
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-10-13 11:35:38 +0100

    Fixed the event registration button

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,6 +1,6 @@
 class EventAttendeesController < ApplicationController
   def join
-    @event_attendee = EventAttendee.new(event_id: params[:id], user_id: session[:user_id])
+    @event_attendee = EventAttendee.new(event_id: params[:id], user_id: current_user.id)
     @event_attendee.save
   end
 end
